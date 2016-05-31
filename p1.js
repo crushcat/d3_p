a949d301c57961ed25faa83557a219bf5e222a45
syshchyk <crushcat@gmail.com>
2016-05-31 08:26:43 +0300

    с1

--- p1.js
+++ p1.js
@@ -5,22 +5,13 @@
 document.getElementById('color').addEventListener('click',colorit);
 
 function colorit() {
-
-    /*d3.select("body").selectAll("p")
+    let divSelection = d3.select('#pcont');
+    let allPs = divSelection.selectAll("p");
+    allPs
         .data([4, 8, 15, 16, 23, 42])
         .enter().append("p")
         .text(function(d) { return "I’m number " + d + "!"; });
-    */
 
-    // Update…
-    var p = d3.select("body").selectAll("p")
-        .data([4, 8, 15, 16, 23, 42])
-        .text(function(d) { return d; });
 
-// Enter…
-    p.enter().append("p")
-        .text(function(d) { return d; });
-
-// Exit…
-    p.exit().remove();
+    d3.select("body").append("svg").attr("width", 50).attr("height", 50).append("circle").attr("cx", 25).attr("cy", 100).attr("r", 25).style("fill", "purple");
 }
